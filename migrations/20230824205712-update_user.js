871b85e970e67e1f2f98808aae069d6aee308d62
maylla04 <maylla.rie@gmail.com>
2023-08-24 17:21:25 -0400

    update_user

--- migrations/20230824205712-update_user.js
+++ migrations/20230824205712-update_user.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-      await queryInterface.addColumn('update_user', 'telephone', {
+      await queryInterface.addColumn('users', 'telephone', {
         type: Sequelize.STRING(11),
         allowNull: true
-      })
+      });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('clientes', 'telephone')
+    await queryInterface.removeColumn('users', 'telephone')
   }
 };
